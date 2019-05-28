18911c31e0077fc610c4a07b1c58e1cd09875bb7
Johan Bové <johan.bove@gmail.com>
2019-05-29 00:25:15 +0200

    Changes preview and head.tpl.php

--- src/templates/default/jbove/shell/head.tpl.php
+++ src/templates/default/jbove/shell/head.tpl.php
@@ -1,1 +1,2 @@
+<link href="https://fonts.googleapis.com/css?family=Asap:400,400i,700|Open+Sans:700" rel="stylesheet">
 <link href="<?php echo \Idno\Core\Idno::site()->config()->getDisplayURL() ?>Themes/Jbove/css/default.min.css" rel="stylesheet">
