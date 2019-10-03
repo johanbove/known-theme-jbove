d2b89b60b5db2715b5ab6fd0edc9d9f6920d6b62
Johan Bové <johan.bove@gmail.com>
2019-10-03 22:28:31 +0200

    Registers footer.tpl and head.tpl

---
+++ src/templates/default/jbove/shell/footer.tpl.php
@@ -0,0 +1,41 @@
+<?php
+$object = $vars['object'];
+
+if (!empty($object)) {
+?>
+<div class="blank-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 u-webring">
+                <p class="text-center">
+                    <a href="https://xn--sr8hvo.ws/%F0%9F%94%B6%E2%80%BC%EF%B8%8F%F0%9F%94%9C/previous" rel="nofolllow noopener">←</a>
+                    An IndieWeb Webring 🕸💍
+                    <a href="https://xn--sr8hvo.ws/%F0%9F%94%B6%E2%80%BC%EF%B8%8F%F0%9F%94%9C/next" rel="nofollow noopener">→</a>
+                </p>
+                <p class="text-center" style="max-width:500px;margin:auto auto 30px;font-style:italic;"><small>The web ring leads to many interesting personal sites worth exploring,
+                        but I am not in control of where the arrows will lead you. Click on your own risk.</small></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 u-server">
+                <p class="text-center"><a href="https://withknown.com/?utm_source=footer&utm_medium=installation" class="u-platform" rel="noopener">Known v0.14.1</a></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 u-badges text-center">
+                <a href="https://indieweb.org/" rel="noopener"><img src="https://johanbove.info/img/badges/indieweb.png" alt="IndieWebCamp" style="image-rendering: pixelated;"
+                        width="80" height="15"></a>
+                <a href="http://microformats.org/" rel="noopener"><img src="https://johanbove.info/img/badges/microformats.png" alt="Microformats"
+                        style="image-rendering: pixelated;" width="80" height="15"></a>
+                <a href="https://indieweb.org/Webmention" rel="noopener"><img src="https://johanbove.info/img/badges/webmention.png" alt="Webmention"
+                        style="image-rendering: pixelated;" width="80" height="15"></a>
+                <a href="https://www.w3.org/html/" rel="noopener"><img src="https://johanbove.info/img/badges/w3c-valid-html.png" alt="W3C HTML5"
+                        style="image-rendering: pixelated;" width="80" height="15"></a>
+                <a rel="license noopener" href="https://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons Licence" style="image-rendering: pixelated;"
+                        src="<?php echo \Idno\Core\Idno::site()->config()->getStaticURL()?>gfx/other/cc_80x15.png"></a></a>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+}
--- src/templates/default/jbove/shell/head.tpl.php
+++ src/templates/default/jbove/shell/head.tpl.php
@@ -1,2 +1,69 @@
+<link rel="microsub" href="https://aperture.p3k.io/microsub/287">
+<!-- Styles -->
 <link href="https://fonts.googleapis.com/css?family=Asap:400,400i,700|Open+Sans:700" rel="stylesheet">
 <link href="<?php echo \Idno\Core\Idno::site()->config()->getDisplayURL() ?>Themes/Jbove/css/default.min.css" rel="stylesheet">
+<style>
+/* Temp CSS hacks supposed to be moved to actual CSS stylesheet some time and day */
+
+/* Tiny screen is for Nokia 8110 4G */
+@media (max-width: 240px) {
+   body {
+      font-size: 12px;
+      padding-top: 50px;
+   }
+   .nabar {
+      margin-bottom: 25px;
+   }
+   .navbar-brand {
+      font-size: 18px;
+   }
+   .navbar-fixed-bottom, .navbar-fixed-top {
+      /* fixes the menu not being scrollable */
+      position: absolute;
+   }
+}
+
+.subscription,
+.navbar-default {
+   background-color: #1a2835;
+}
+
+.idno-content {
+   box-shadow: -1px -1px 1px rgba(255,255,255,0.1);
+}
+
+.pager li a {
+   background-color: #4c93cb;
+   color: #fff;
+   border: 3px solid #73b2e3;
+}
+
+.page-body .row p a {
+   text-decoration: underline;
+}
+
+.navbar-nav > li > .nav > li >a:focus,
+.navbar-nav > li > .nav > li > a:hover {
+   color: #4c93cb;
+}
+
+.reply-text p a,
+a {
+    color: inherit;
+    font-weight: 600;
+}
+
+@media (max-width: 767px) {
+   .navbar-default .navbar-nav .open .dropdown-menu>li>a {
+       color: #ddd;
+   }
+}
+
+blockquote {
+   border-left: 3px solid #fbfbfb;
+}
+
+.idno-body blockquote {
+   font-style: oblique;
+}
+</style>
